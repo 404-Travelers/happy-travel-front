a1eaabdb54ca1d3cd06d1027fe297b68f32bb9b2
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-30 02:51:38 +0200

    feat(trip-page): improve buttons and security

--- src/app/components/tripForm/tripForm.js
+++ src/app/components/tripForm/tripForm.js
@@ -1,10 +1,10 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import styles from "./destinationForm.module.css";
+import styles from './tripForm.module.css'
 import { TripsService } from "@/services/tripsService";
 
-export default function tripForm({ id, initialData }) {
+export default function TripForm({ id, initialData }) {
     const api = TripsService();
     const [form, setForm] = useState({
         country: "",
